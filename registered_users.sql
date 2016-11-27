63dd528a9298e69148284e9e63c9927ce9c74a97
dhanapalms <sdhanapalms@gmail.com>
2016-11-27 19:02:50 +0530

    Add files via upload

--- registered_users.sql
+++ registered_users.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4.1
+-- version 3.3.9
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 22, 2014 at 12:48 PM
--- Server version: 5.5.32
--- PHP Version: 5.4.19
+-- Host: localhost
+-- Generation Time: Nov 27, 2016 at 01:32 PM
+-- Server version: 5.1.53
+-- PHP Version: 5.3.4
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -17,7 +16,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `phppot_examples`
+-- Database: `event`
 --
 
 -- --------------------------------------------------------
@@ -27,16 +26,24 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `registered_users` (
-  `id` int(8) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(255) NOT NULL,
-  `first_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
-  `password` varchar(25) NOT NULL,
-  `email` varchar(55) NOT NULL,
-  `gender` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `name` varchar(25) NOT NULL,
+  `eventname` varchar(50) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `eventdate` date NOT NULL,
+  `email` varchar(40) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Dumping data for table `registered_users`
+--
+
+INSERT INTO `registered_users` (`name`, `eventname`, `description`, `location`, `eventdate`, `email`) VALUES
+('dhanapal', 'Animation ', 'VFX animated', 'SJT', '2016-11-29', 'sdhanapalms@gmail.com'),
+('abc', 'disq', 'sdfs', 'vit', '2016-11-02', 'sdhan@gmail.com'),
+('balaji', 'ABC', 'ABCDHEBHD', 'VELLORE', '2016-11-11', 'balaji@gmail.com'),
+('balaji', 'ABC', 'ABCDHEBHD', 'VELLORE', '2016-11-11', 'balaji@gmail.com'),
+('jsdbcjsd', 'bsjcbc', 'sdfsdf', 'sfsdfdf', '2016-11-11', 'dsadasdasd@gmail.com'),
+('naren', 'vfx', 'abxsgde', 'vellore', '2016-11-29', 'gsrs.1311@gmail.com'),
+('naren', 'vfx', 'abxsgde', 'vellore', '2016-11-29', 'gsrs.1311@gmail.com'),
+('abcdef', 'phone', 'asdsdadsd', 'vit', '2016-11-30', 'sdhshd@gmail.com');
